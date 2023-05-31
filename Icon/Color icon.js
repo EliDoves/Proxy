776dba413bd818cf44dcf1f8d09599a77c451613
吉川由紀 <51412618+EliDoves@users.noreply.github.com>
2023-05-31 17:31:35 +0800

    Add Color icons

---
+++ Icon/Color icon.js
@@ -0,0 +1,9 @@
+{"name":"Color icon", "description":"By EliDoves", "icons":[{"name":"OpenAI", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/main/Icon/Color/OpenAI.png"}, {"name":"Amazon", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/main/Icon/Color/Amazon.png"}, {"name":"LINE", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/main/Icon/Color/LINE.png"}, {"name":"Microsoft", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/main/Icon/Color/Microsoft.png"}, {"name":"Onedrive", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/main/Icon/Color/Onedrive.png"}, {"name":"Paypal", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/main/Icon/Color/Paypal.png"}, {"name":"Telegram", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/main/Icon/Color/Telegram.png"}, {"name":"Twitter", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/main/Icon/Color/Twitter.png"}, {"name":"windows_11", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/main/Icon/Color/windows_11.png"}, {"name":"MEGA", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/main/Icon/Color/MEGA.png"}, {"name":"YouTube", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/main/Icon/Color/YouTube.png"}]}
+
+
+
+
+
+
+
+
