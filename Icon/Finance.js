7e1b7de154769e47d8670354dbeaf27bd8526f01
吉川由紀 <51412618+EliDoves@users.noreply.github.com>
2025-08-09 21:59:22 +0800

    Update Finance.js

--- Icon/Finance.js
+++ Icon/Finance.js
@@ -9,6 +9,7 @@
     {"name":"Wise", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/refs/heads/main/Icon/Finance/Wise.png"},
     {"name":"iFastBank", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/refs/heads/main/Icon/Finance/iFastBank.png"},
     {"name":"LongBridge", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/refs/heads/main/Icon/Finance/LongBridge.png"},
-    {"name":"TNGWallet", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/refs/heads/main/Icon/Finance/TNGWallet.png"},    
+    {"name":"TNGWallet", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/refs/heads/main/Icon/Finance/TNGWallet.png"}, 
+    {"name":"富途证券", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/refs/heads/main/Icon/Finance/富途证券.png"},  
  ]
 }
