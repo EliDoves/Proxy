e61a147b24f6dd12ebd95957bce50666734d70b3
吉川由紀 <51412618+EliDoves@users.noreply.github.com>
2025-08-09 21:50:44 +0800

    Update Finance.js

--- Icon/Finance.js
+++ Icon/Finance.js
@@ -6,6 +6,6 @@
     {"name":"OCBC", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/refs/heads/main/Icon/Finance/OCBC.png"},
     {"name":"HSBC", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/refs/heads/main/Icon/Finance/HSBC.png"},
     {"name":"PayPay", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/refs/heads/main/Icon/Finance/PayPay.png"},
-    {"name":"WISE", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/refs/heads/main/Icon/Finance/WISE.png"},
+    {"name":"WISE1", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/refs/heads/main/Icon/Finance/WISE1.png"},
  ]
 }
