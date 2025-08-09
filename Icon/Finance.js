8a76594377b4def91f98cf3693c16c721214e7e1
吉川由紀 <51412618+EliDoves@users.noreply.github.com>
2025-08-09 21:44:27 +0800

    Update Finance.js

--- Icon/Finance.js
+++ Icon/Finance.js
@@ -1,1 +1,7 @@
-
+{
+  "name":"Finance", "description":"Finance平台图标", 
+  "icons":[    
+    {"name":"Component1", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/refs/heads/main/Icon/Finance/IBKR.png"},
+    {"name":"Component2", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/refs/heads/main/Icon/Finance/Shopify.png"},
+ ]
+}
