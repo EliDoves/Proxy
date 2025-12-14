81c97bd60385f7418e0e87b50a8f1f962ae9af0f
きたいくよ <51412618+EliDoves@users.noreply.github.com>
2025-12-14 20:46:17 +0800

    Create Others.js

---
+++ Icon/Others.js
@@ -0,0 +1,7 @@
+{
+  "name":"Others", "description":"一些好看的图标", 
+  "icons":[    
+    {"name":"yamada ryo", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/refs/heads/main/Icon/Other/yamada ryo.png"},
+    {"name":"kita ikuyo", "url":"https://raw.githubusercontent.com/EliDoves/Proxy/refs/heads/main/Icon/Other/kita ikuyo.png"},
+ ]
+}
